ea57355a39d424465a3182503350a637cb71e8ff
Jonathan Clark <jonny@pop-os.localdomain>
2020-07-15 14:32:20 -0700

    working on 9

--- 2.js
+++ 2.js
@@ -23,10 +23,91 @@ function main() {
 
   //4.
   console.log(arr.get(0));
-  arr = [];
+  arr.pop();
+  arr.pop();
+  arr.pop();
+
   console.log(arr);
   arr.push('tauhida');
   console.log(arr);
 }
 
+//5.
+function urlify(string) {
+  return string.split(' ').join('%20');
+}
+
+//6.
+function filterNums(arrayOfNums) {
+  result = [];
+  for (i = 0; i < arrayOfNums.length; i++) {
+    if (arrayOfNums[i] >= 5) {
+      result.push(arrayOfNums[i]);
+      //   break;  <- return the first value in array larger than or equal to 5
+    }
+  }
+  return result;
+}
+
+//7.
+function maxSum(numArray) {
+  let sumOfNums = [0];
+  //Record the sum of each item in array
+  for (i = 0; i < numArray.length; i++) {
+    result = sumOfNums[i] + numArray[i];
+    sumOfNums.push(result);
+  }
+  console.log(sumOfNums);
+  return Math.max(...sumOfNums);
+}
+
+//8.
+function mergeArrays(a, b) {
+  const newArray = [];
+  let arrayIndexA = 0;
+  let arrayIndexB = 0;
+
+  while (newArray.length < a.length + b.length) {
+    if (a[arrayIndexA] < b[arrayIndexB] || !b[arrayIndexB]) {
+      newArray.push(a[arrayIndexA]);
+      arrayIndexA++;
+    } else {
+      newArray.push(b[arrayIndexB]);
+      arrayIndexB++;
+    }
+  }
+  return newArray;
+}
+
+//9.
+function removeChars(string, chars) {
+  const oldString = [...string];
+  const charsToRemove = [...chars];
+
+  let newString = '';
+  //   const finalString = '';
+
+  for (let i = 0; i < oldString.length; i++) {
+    for (let j = 0; j < charsToRemove.length; j++) {
+      if (oldString[i] != charsToRemove[j]) {
+        console.log(oldString[i]);
+        console.log(charsToRemove[j]);
+        // newString.push(oldString[i]);
+        newString += oldString[i];
+        break;
+      }
+    }
+  }
+  //   return finalString.concat(newString);
+  return newString;
+}
+
 main();
+console.log(urlify('tauhida parveen'));
+console.log(urlify('www.thinkful.com /tauh ida parv een'));
+console.log(filterNums([1, 8, 4, 6, 3, 7, 8]));
+console.log(maxSum([4, 6, -3, 5, -2, 1]));
+
+console.log(mergeArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+
+console.log(removeChars('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'));
